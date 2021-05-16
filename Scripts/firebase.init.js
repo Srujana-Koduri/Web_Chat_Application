3f29e1272b2e0c380b5657768bf1c5d0fa3e0ed1
Srujana <17211a05d0@bvrit.ac.in>
2021-05-16 10:40:34 +0530

    pushing application

---
+++ Scripts/firebase.init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDUi3pQhWikuoycWIaYuHF5MQ628GL5ppI",
+    authDomain: "chat-app-2edc6.firebaseapp.com",
+    databaseURL: "https://chat-app-2edc6.firebaseio.com",
+    projectId: "chat-app-2edc6",
+    storageBucket: "chat-app-2edc6.appspot.com",
+    messagingSenderId: "978813595455",
+    appId: "1:978813595455:web:4f4b3f4127674cb2306d8a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
